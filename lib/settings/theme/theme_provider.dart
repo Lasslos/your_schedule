1e546f97238e313d0084a7e8771b37e5751d5c21
lasloh <laslo.hauschild@web.de>
2022-12-25 17:34:58 +0100

    ThemeMode null safety

--- lib/settings/theme/theme_provider.dart
+++ lib/settings/theme/theme_provider.dart
@@ -2,15 +2,14 @@ import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 
-StateNotifierProvider<ThemeProvider, ThemeMode?> themeProvider =
-    StateNotifierProvider<ThemeProvider, ThemeMode?>((ref) => ThemeProvider());
+StateNotifierProvider<ThemeProvider, ThemeMode> themeProvider =
+    StateNotifierProvider<ThemeProvider, ThemeMode>((ref) => ThemeProvider());
 
-class ThemeProvider extends StateNotifier<ThemeMode?> {
-  ThemeProvider() : super(null);
+class ThemeProvider extends StateNotifier<ThemeMode> {
+  ThemeProvider() : super(ThemeMode.system);
 
-  void setTheme(ThemeMode? theme) {
-    state = theme;
-    if (theme != null) {
+  void setTheme(ThemeMode theme) {
+    if (theme != ThemeMode.system) {
       SharedPreferences.getInstance().then((prefs) {
         prefs.setBool('darkMode', theme == ThemeMode.dark);
       });
