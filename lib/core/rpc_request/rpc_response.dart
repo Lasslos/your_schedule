3211489493ff5a213af6723c063905305e623b9e
lasloh <laslo.hauschild@web.de>
2024-02-08 15:16:25 +0100

    Made RPCResponse sealed

--- lib/core/rpc_request/rpc_response.dart
+++ lib/core/rpc_request/rpc_response.dart
@@ -5,22 +5,20 @@ import 'package:your_schedule/core/rpc_request/rpc.dart';
 part 'rpc_response.freezed.dart';
 
 @Freezed(fromJson: false, toJson: false)
-class RPCResponse with _$RPCResponse {
+sealed class RPCResponse with _$RPCResponse {
   @Assert('jsonrpc == "2.0"', 'jsonrpc must be exactly "2.0"')
   @Assert('result != null', 'result must not be null')
   const factory RPCResponse.result(
     @protected String jsonrpc,
     dynamic result,
-    String id,
-  ) = _RPCResponse;
+    String id,) = RPCResponseResult;
 
   @Assert('jsonrpc == "2.0"', 'jsonrpc must be exactly "2.0"')
   @Assert('error != null', 'error must not be null')
   const factory RPCResponse.error(
     @protected String jsonrpc,
     RPCError error,
-    String id,
-  ) = _RPCResponseError;
+    String id,) = RPCResponseError;
 
   factory RPCResponse.fromJson(Map<String, dynamic> json) {
     if (json['error'] != null) {
